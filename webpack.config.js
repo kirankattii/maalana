84101ef1e12caf8a8b032707251feca159057d8b
kiran katti <kirankatti550@gmail.com>
2024-07-22 12:07:34 +0530

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,17 @@
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer")
+const TerserPlugin = require("terser-webpack-plugin")
 
 module.exports = {
-	plugins: [new BundleAnalyzerPlugin()],
+	optimization: {
+		minimize: true,
+		minimizer: [new TerserPlugin()],
+	},
+	plugins: [
+		new BundleAnalyzerPlugin({
+			analyzerMode: "static",
+			openAnalyzer: false,
+			generateStatsFile: true,
+			statsOptions: { source: false },
+		}),
+	],
 }
